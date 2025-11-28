a47c5e19f30c23ae36577daa156afaa3e4b884eb
Evertondiasss <evertondiasss@hotmail.com>
2025-11-28 09:05:20 -0300

    Configuração de login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 // backend/controllers/auth.controller.js
 import pool from "../config/db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // ================================
 // POST /api/login
@@ -29,11 +30,22 @@ export async function login(req, res) {
       return res.status(401).json({ error: "Usuário ou senha inválidos." });
     }
 
+    // ================================
+    // GERA TOKEN JWT
+    // ================================
+    const token = jwt.sign(
+      { id: user.id, login: user.login },
+      process.env.JWT_SECRET,
+      { expiresIn: "8h" }
+    );
+
     return res.json({
       id: user.id,
       login: user.login,
       nome: user.nome,
+      token,
     });
+
   } catch (e) {
     console.error("POST /api/login ERRO:", e.code, e.sqlMessage || e.message);
     return res.status(500).json({ error: "Erro interno no servidor." });
