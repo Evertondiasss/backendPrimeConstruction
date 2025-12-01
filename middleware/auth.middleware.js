20d38562df826540e64927cf5abcb1f6dc5f86e8
Evertondiasss <evertondiasss@hotmail.com>
2025-12-01 10:38:34 -0300

    ajustes arquivo middleware.js

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export function requireAuth(req, res, next) {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Não autorizado" });
+  }
+
+  const token = authHeader.substring(7);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Token inválido ou expirado" });
+  }
+}
